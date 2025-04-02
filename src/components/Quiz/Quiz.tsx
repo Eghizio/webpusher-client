cbf6104499917643064aa4c2315884e490fb56ea
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-04-02 20:46:24 +0200

    Add quiz placeholder

--- src/components/Quiz/Quiz.tsx
+++ src/components/Quiz/Quiz.tsx
@@ -4,5 +4,28 @@ import { Title } from "../Title/Title";
 interface Props {}
 
 export const Quiz = ({}: Props) => {
-  return <Title left={<Trophy size={26} />}>Quiz</Title>;
+  return (
+    <article className="flex flex-col gap-2">
+      <Title left={<Trophy size={26} />}>Quiz</Title>
+
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem
+        molestiae, eum, eius enim voluptas error praesentium ab sint officia quo
+        corrupti eaque! Accusantium at ducimus minus molestias obcaecati nam
+        optio?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem
+        molestiae, eum, eius enim voluptas error praesentium ab sint officia quo
+        corrupti eaque! Accusantium at ducimus minus molestias obcaecati nam
+        optio?
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem
+        molestiae, eum, eius enim voluptas error praesentium ab sint officia quo
+        corrupti eaque! Accusantium at ducimus minus molestias obcaecati nam
+        optio?
+      </p>
+    </article>
+  );
 };
