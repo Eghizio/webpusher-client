2a372fb41c0937f9df1bb8fe01a7ffb220df7c80
Jakub Wąsik <32049761+Eghizio@users.noreply.github.com>
2025-04-03 03:20:38 +0200

    Configure TRQ cache

--- src/hooks/useUsers.ts
+++ src/hooks/useUsers.ts
@@ -1,8 +1,11 @@
 import { Api } from "@/api/Api";
 import { useQuery } from "@tanstack/react-query";
 
+const HALF_MINUTE_ms = 30 * 1_000;
+
 export const useUsers = () =>
   useQuery({
     queryKey: ["users"],
     queryFn: Api.getUsers,
+    gcTime: HALF_MINUTE_ms,
   });
